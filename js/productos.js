b509f484a4481b94d98a68d894ce7d32b2f4328c
Aurora <auro.prz1809@gmail.com>
2023-05-15 19:05:45 -0600

    Al categories are completed

--- js/productos.js
+++ js/productos.js
@@ -1,36 +1,47 @@
 document.addEventListener('DOMContentLoaded', (event) =>{
+    console.log("El valor del parámetro es: " + category);
     JsonParaLocalStorage()
       .then(() => {
         let datos = JSON.parse(localStorage.getItem('products'));
         Object.entries(datos).forEach(([key, value])=>{
-            var photo = ""
-            if(value.Category=='Dairy Products'){
-              photo = "Dairy-products.jpg"
-            }else if(value.Category=='Beverages'){
-              photo = "refrescos.png"
-            }else if(value.Category=='Meat/Poultry'){
-              photo = "Meat.jpg"
-            }else if(value.Category=='Seafood'){
-              photo = "seafood.jpg"
+            if(category=='category1' && value.Category!='Dairy Products'){
+              
+            }else if(category=='category2' && value.Category!='Beverages'){
+
+            }else if(category=='category3' && value.Category!='Meat/Poultry'){
+
+            }else if(category=='category4' && value.Category!='Seafood'){
+
+            }else{
+                var photo = ""
+                if(value.Category=='Dairy Products'){
+                  photo = "Dairy-products.jpg"
+                }else if(value.Category=='Beverages'){
+                  photo = "refrescos.png"
+                }else if(value.Category=='Meat/Poultry'){
+                  photo = "Meat.jpg"
+                }else if(value.Category=='Seafood'){
+                  photo = "seafood.jpg"
+                }
+                var div = document.createElement('div');
+                div.setAttribute('class', "card col-12 col-md-6 col-lg-3")
+                div.setAttribute('style', 'width: 18rem;')
+                div.innerHTML = `
+                    <img src="./files/${photo}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <div class="row">
+                            <p class="card-text col-6 categoria">${value.Category}</p>
+                            <p class="card-text col-6 stock">STOCK: ${value.UnitsInStock}</p>
+                        </div>
+                        <h5 class="card-title">${value.ProductName}</h5>
+                        <p class="precio">$${value.UnitPrice}</p>
+                        <div class="btn-section-card">
+                            <a href="#" class="btn btn-add">+</a>
+                        </div>
+                    </div>
+                `;
+                document.getElementById('products').appendChild(div);
             }
-            var div = document.createElement('div');
-            div.setAttribute('class', "card col-12 col-md-6 col-lg-3")
-            div.setAttribute('style', 'width: 18rem;')
-            div.innerHTML = `
-                <img src="./files/${photo}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <div class="row">
-                        <p class="card-text col-6 categoria">${value.Category}</p>
-                        <p class="card-text col-6 stock">STOCK: ${value.UnitsInStock}</p>
-                    </div>
-                    <h5 class="card-title">${value.ProductName}</h5>
-                    <p class="precio">$${value.UnitPrice}</p>
-                    <div class="btn-section-card">
-                        <a href="#" class="btn btn-add">+</a>
-                    </div>
-                </div>
-            `;
-            document.getElementById('products').appendChild(div);
         })
       })
       .catch(error => console.error(error));
